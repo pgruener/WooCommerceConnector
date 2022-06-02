bb26ccd0c123e443ac59bae4e9e7b6de20e18921
gruener <philipp.gruener@royal-software.de>
2022-06-02 21:36:58 +0200

    Update sync_orders.py
    In Sales Order there is no posting_date -> it's the transaction_date

--- woocommerceconnector/sync_orders.py
+++ woocommerceconnector/sync_orders.py
@@ -216,7 +216,7 @@ def create_sales_order(woocommerce_order, woocommerce_settings, company=None):
             "taxes_and_charges": tax_rules,
             "customer_address": billing_address,
             "shipping_address_name": shipping_address,
-            "posting_date": woocommerce_order.get("date_created")[:10]          # pull posting date from WooCommerce
+            "transaction_date": woocommerce_order.get("date_created")[:10]          # pull posting date from WooCommerce
         })
 
         so.flags.ignore_mandatory = True
